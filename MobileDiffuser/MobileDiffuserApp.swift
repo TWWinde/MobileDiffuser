eedd60300228e3e600fc61e553a5c98c1ccd8616
TWWinde <st180408@stud.uni-stuttgart.de>
2026-03-17 14:31:52 +0100

    Initial Commit

---
+++ MobileDiffuser/MobileDiffuserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDiffuserApp.swift
+//  MobileDiffuser
+//
+//  Created by Tang Wenwu on 17.03.26.
+//
+
+import SwiftUI
+
+@main
+struct MobileDiffuserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
